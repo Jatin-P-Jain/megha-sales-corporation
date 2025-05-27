16d31481d490200f170f2918286bb5c25b81adf4
Jatin Prakash Jain <jatin.jain@zapcg.com>
2025-05-27 16:57:24 +0530

    refactor: remove modal prop from layout component to simplify structure

--- src/app/cart/layout.tsx
+++ src/app/cart/layout.tsx
@@ -1,14 +1,11 @@
 export default function layout({
   children,
-  modal,
 }: {
   children: React.ReactNode;
-  modal: React.ReactNode;
 }) {
   return (
     <div className="mx-auto h-[100%] max-w-screen-lg">
       {children}
-      {modal}
     </div>
   );
 }
