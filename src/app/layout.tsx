49952dc612f974fa9ce2daa279bb502b68117f25
Jatin Prakash Jain <jatin.jain@zapcg.com>
2025-06-13 17:37:33 +0530

    fix: update middleware to log triggered requests and ensure manifest.json is correctly referenced

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ export const metadata: Metadata = {
   title: "Megha Sales Corporation",
   description: "Auto accessories store in Raipur, Chhattisgarh",
   icons: "/brand-logo.svg",
+  manifest:'/manifest.json'
 };
 
 export default async function RootLayout({
@@ -30,7 +31,7 @@ export default async function RootLayout({
         content="width=device-width, initial-scale=1, height=device-height"
       />
       <head>
-        <link rel="manifest" href="/manifest.json" />
+        {/* <link rel="manifest" href="/manifest.json" /> */}
         <link
           rel="icon"
           type="image/png"
