82090aeda93dfa47fd5939e940a15accc29ba011
Jatin Prakash Jain <jatin.jain@zapcg.com>
2025-08-03 15:40:21 +0530

    feat: integrate user deletion functionality in account management

--- src/app/account/actions.ts
+++ src/app/account/actions.ts
@@ -14,8 +14,26 @@ export const updateUser = async ({
     const userRef = fireStore.collection("users").doc(userId);
     console.log({ userRef });
     const imageUrl = imageUrlFormatter(photoUrl);
-    userRef.update({ photoUrl: imageUrl });
+    await userRef.update({ photoUrl: imageUrl });
   } catch (error) {
     console.log("Error while updating the user -- ", { error });
+  }
+};
+export const deleteUserCart = async ({ userId }: { userId: string }) => {
+  if (!userId) return;
+  try {
+    const cartRef = fireStore.collection("carts").doc(userId);
+    fireStore.recursiveDelete(cartRef);
+  } catch (error) {
+    console.log("Error while deleting cart for user -- ", { error });
+  }
+};
+export const deleteUserData = async ({ userId }: { userId: string }) => {
+  if (!userId) return;
+  try {
+    const userRef = fireStore.collection("users").doc(userId);
+    await userRef.delete();
+  } catch (error) {
+    console.log("Error while deleting user data-- ", { error });
   }
 };
