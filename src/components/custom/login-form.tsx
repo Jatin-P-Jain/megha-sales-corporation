ada2f7e27a701469ad9059532ee88d9137262505
Jatin Prakash Jain <jatin.jain@zapcg.com>
2025-08-04 13:08:35 +0530

    feat: enhance login and authentication experience with help dialog and PWA prompts

--- src/components/custom/login-form.tsx
+++ src/components/custom/login-form.tsx
@@ -1,32 +1,41 @@
 "use client";
-import Link from "next/link";
 import GoogleLoginButton from "@/components/custom/google-login-button";
 
 import CollapsibleLoginForm from "./collapsible-login-form";
 
 import { MobileAuthWrapper } from "./mobile-auth/mobile-auth-wrapper";
+import { Separator } from "../ui/separator";
 
 export default function LoginForm({ onSuccess }: { onSuccess?: () => void }) {
   return (
     <div>
       <MobileAuthWrapper onSuccess={onSuccess} />
-      <span className="my-4 flex w-full justify-center text-[14px] text-zinc-500">
-        or
-      </span>
+      <div className="relative mx-8 my-4 flex flex-row items-center justify-center gap-4 overflow-hidden md:mx-36 md:gap-8">
+        <Separator />
+        <span className="text-muted-foreground text-[14px]">or</span>
+        <Separator />
+      </div>
       <GoogleLoginButton
         variant="outline"
         onSuccess={onSuccess}
         isLogin={true}
       />
-      <span className="my-4 flex w-full justify-center text-[14px] text-zinc-500">
-        or
-      </span>
+      <div className="relative mx-8 my-4 flex flex-row items-center justify-center gap-4 overflow-hidden md:mx-36 md:gap-8">
+        <Separator />
+        <span className="text-muted-foreground text-[14px]">or</span>
+        <Separator />
+      </div>
       <CollapsibleLoginForm />
-      <div className="mt-4 flex items-center justify-center gap-2 text-xs md:text-sm">
+      {/* <div className="mt-4 flex items-center justify-center gap-2 text-xs md:text-sm">
         Don&apos;t have an account?
         <Link href="/register" className="text-cyan-900 underline">
           Register here.
         </Link>
+      </div> */}
+      <div className="text-muted-foreground mt-4 text-justify text-xs italic">
+        If you don&apos;t have an account with us yet, please log in to begin.
+        We will then kindly ask you to provide a few details to complete your
+        registration. Thank you!
       </div>
       <div id="recaptcha-container" className="opacity-0" />
     </div>
